e70b73afe2d2d20bc9d61c557b05822ba78481d7
m-pranavraj <mogantipranavraj@gmail.com>
2026-05-02 16:52:53 +0530

    Convert to standard Vite SPA to fix 404 on Vercel

--- vite.config.ts
+++ vite.config.ts
@@ -3,11 +3,8 @@ import { tanstackRouter } from "@tanstack/router-plugin/vite";
 import react from "@vitejs/plugin-react";
 import tsconfigPaths from "vite-tsconfig-paths";
 import tailwindcss from "@tailwindcss/vite";
-import { tanstackStart } from "@tanstack/react-start/plugin/vite";
-
 export default defineConfig({
   plugins: [
-    tanstackStart(),
     tanstackRouter(),
     react(),
     tailwindcss(),
